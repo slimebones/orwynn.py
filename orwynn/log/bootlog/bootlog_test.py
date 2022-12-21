f99c87f20d799021b57905bbbbdcca48ae10a54f
ryzhovalex <thed4rkof@gmail.com>
2022-12-21 12:37:38 +0400

    Move capture_stdout

--- orwynn/log/bootlog/bootlog_test.py
+++ orwynn/log/bootlog/bootlog_test.py
@@ -1,9 +1,7 @@
-import io
 import re
-from contextlib import redirect_stdout
-from typing import Callable
 
 from orwynn.log.bootlog.Bootlog import Bootlog
+from orwynn.util import uio
 
 _MESSAGE: str = "hello world!"
 _LOG_MATCH_PATTERN: str = \
@@ -11,38 +9,36 @@ _LOG_MATCH_PATTERN: str = \
     + r" - \[Boot \/ .+(DEBUG|INFO|WARNING|ERROR|CRITICAL).+\] .+"
 
 
-def _get_stdout(fn: Callable, *args, **kwargs) -> str:
-    stringio = io.StringIO()
-    with redirect_stdout(stringio):
-        fn(*args, **kwargs)
-    return stringio.getvalue()
-
-
 def test_debug():
     assert \
-        re.match(_LOG_MATCH_PATTERN, _get_stdout(Bootlog.debug, _MESSAGE)) \
-        is not None
+        re.match(_LOG_MATCH_PATTERN, uio.capture_stdout(
+            Bootlog.debug, _MESSAGE
+        )) is not None
 
 
 def test_info():
     assert \
-        re.match(_LOG_MATCH_PATTERN, _get_stdout(Bootlog.info, _MESSAGE)) \
-        is not None
+        re.match(_LOG_MATCH_PATTERN, uio.capture_stdout(
+            Bootlog.info, _MESSAGE
+        )) is not None
 
 
 def test_warning():
     assert \
-        re.match(_LOG_MATCH_PATTERN, _get_stdout(Bootlog.warning, _MESSAGE)) \
-        is not None
+        re.match(_LOG_MATCH_PATTERN, uio.capture_stdout(
+            Bootlog.warning, _MESSAGE
+        )) is not None
 
 
 def test_error():
     assert \
-        re.match(_LOG_MATCH_PATTERN, _get_stdout(Bootlog.error, _MESSAGE)) \
-        is not None
+        re.match(_LOG_MATCH_PATTERN, uio.capture_stdout(
+            Bootlog.error, _MESSAGE
+        )) is not None
 
 
 def test_critical():
     assert \
-        re.match(_LOG_MATCH_PATTERN, _get_stdout(Bootlog.critical, _MESSAGE)) \
-        is not None
+        re.match(_LOG_MATCH_PATTERN, uio.capture_stdout(
+            Bootlog.critical, _MESSAGE
+        )) is not None
