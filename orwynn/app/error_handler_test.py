d1d965a5c606780db7ad97d58bf468e63b550827
ryzhovalex <thed4rkof@gmail.com>
2022-12-23 15:38:31 +0400

    Finish with error handlers

--- orwynn/app/error_handler_test.py
+++ orwynn/app/error_handler_test.py
@@ -60,3 +60,40 @@ def test_default_exception():
     )
 
     assert recovered_exception.args[0] == "whoops!"
+
+
+def test_identical_error_handlers():
+    class C1(Controller):
+        ROUTE = "/"
+        METHODS = ["get"]
+
+        def get(self):
+            raise Error("whoops!")
+
+    class EH1(ErrorHandler):
+        E = Error
+
+        def handle(self, request: Request, error: Error):
+            return JSONResponse(error.api, 400)
+
+    class EH2(ErrorHandler):
+        E = Error
+
+        def handle(self, request: Request, error: Error):
+            return JSONResponse(error.api, 401)
+
+    boot: Boot = Boot(
+        Module(route="/", Controllers=[C1]),
+        # Order matters
+        ErrorHandlers=[EH1, EH2]
+    )
+    http: HttpClient = boot.app.http_client
+
+    r: TestResponse = http.get("/", 401)
+
+    recovered_error: Error = validation.apply(
+        BootProxy.ie().api_indication.recover(r.json()),
+        Error
+    )
+
+    assert recovered_error.message == "whoops!"
