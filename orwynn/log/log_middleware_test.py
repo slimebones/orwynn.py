2ef0b7f3e29e96c46e9dda151d7e2eec0ca5c728
ryzhovalex <thed4rkof@gmail.com>
2023-02-10 17:55:28 +0400

    test: Add log error test

--- orwynn/log/log_middleware_test.py
+++ orwynn/log/log_middleware_test.py
@@ -1,18 +1,35 @@
 import json
+from typing import Callable
 
 from loguru._handler import Message
 
 from orwynn.boot.Boot import Boot
 from orwynn.controller.endpoint.Endpoint import Endpoint
 from orwynn.controller.http.HTTPController import HTTPController
+from orwynn.error.Error import Error
 from orwynn.error.MalfunctionError import MalfunctionError
 from orwynn.module.Module import Module
 from orwynn.test.Client import Client
 
 
+def __get_log_apprc(check_fn: Callable) -> dict:
+    return {
+        "prod": {
+            "Log": {
+                "handlers": [
+                    {
+                        "sink": check_fn,
+                        "serialize": True
+                    }
+                ]
+            }
+        }
+    }
+
+
 def __check_log_message(message: Message) -> dict:
     data: dict = json.loads(str(message))
-    extra: dict = data["extra"]
+    extra: dict = data["record"]["extra"]
 
     assert extra["type"] in ["request", "response"]
     assert type(extra["headers"]) is dict
@@ -49,28 +66,45 @@ def test_get():
         data: dict = __check_log_message(message)
         json: dict | None = data["extra"]["json"]
 
-        if data["extra"]["type"] == "request":
+        if data["record"]["extra"]["type"] == "request":
             assert json is None
-        elif data["extra"]["type"] == "response":
+        elif data["record"]["extra"]["type"] == "response":
             assert json == RETURNED_DATA
 
     client: Client = Boot(
         Module(route="/", Controllers=[C1]),
-        apprc={
-            "prod": {
-                "Log": {
-                    "handlers": [
-                        {
-                            "sink": __check,
-                            "serialize": True
-                        }
-                    ]
-                }
-            }
-        }
+        apprc=__get_log_apprc(__check)
     ).app.client
 
     client.get(
         "/",
         200
+    )
+
+
+def test_get__error():
+    class C1(HTTPController):
+        ROUTE = "/"
+        ENDPOINTS = [Endpoint(method="get")]
+
+        def get(self) -> dict:
+            raise Error("hello")
+
+    def __check(message: Message) -> None:
+        data: dict = __check_log_message(message)
+        json: dict | None = data["extra"]["json"]
+
+        if data["extra"]["type"] == "request":
+            assert json is None
+        elif data["extra"]["type"] == "response":
+            assert json == Error("hello").api
+
+    client: Client = Boot(
+        Module(route="/", Controllers=[C1]),
+        apprc=__get_log_apprc(__check)
+    ).app.client
+
+    client.get(
+        "/",
+        400
     )
