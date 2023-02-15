2681a8e17288184703cfd229b5018b3f622a45a1
ryzhovalex <thed4rkof@gmail.com>
2023-02-15 10:29:31 +0400

    fix: Middleware dispatch patterns

--- orwynn/middleware/Middleware.py
+++ orwynn/middleware/Middleware.py
@@ -1,3 +1,4 @@
+import re
 from typing import Any, Callable
 
 from orwynn import validation
@@ -33,7 +34,38 @@ class Middleware:
             )
 
     def __should_process(self, route: str) -> bool:
-        return self.__is_all_routes_allowed or route in self.__covered_routes
+        if self.__is_all_routes_allowed:
+            return True
+
+        for allowed_route in self.__covered_routes:
+            # FIXME: Here in routes other regex-incompatible symbols may
+            #   appear, but i don't know which ones at this point. Maybe we
+            #   should add more advanced adjustments (replacements) or error
+            #   raising.
+            #
+            # Replace variable brackets and dashes from kebab-style notations.
+            #
+            # Also, notice that for paths like "/users/10ci/...." allowed route
+            # "/users/{id}" will be also compliant, which may not seemed right
+            # but due to external checks before route processed to middleware
+            # such types of routes won't appear here (i.e. "/users/10ci/foo"
+            # won't be passed to the controller "/users/{id}").
+            prepared_route: str = re.sub(
+                r"\{.+\}", ".+", allowed_route
+            ).replace(
+                "-", r"\-"
+            )
+
+            route_pattern: re.Pattern = re.compile(
+                # This way paths will be covered too -
+                # e.g. "/file/{}" matches "/file/home/alex/..."
+                prepared_route
+            )
+
+            if route_pattern.fullmatch(route):
+                return True
+
+        return False
 
     async def dispatch(
         self,
--- orwynn/middleware/http_middleware_test.py
+++ orwynn/middleware/http_middleware_test.py
@@ -9,15 +9,19 @@ from orwynn.testing.Client import Client
 from orwynn.web import Request, Response, TestResponse
 
 
-def test_basic():
-    class Mw1(HttpMiddleware):
-        async def process(
-            self, request: Request, call_next: Callable
-        ) -> Response:
-            response: Response = await call_next(request)
-            response.headers["x-test"] = "hello"
-            return response
+class Mw1(HttpMiddleware):
+    async def process(
+        self, request: Request, call_next: Callable
+    ) -> Response:
+        response: Response = await call_next(request)
+        response.headers["x-test"] = "hello"
+        return response
 
+
+def test_basic():
+    """
+    Should work in general cases.
+    """
     class C1(HttpController):
         ROUTE = "/"
         ENDPOINTS = [Endpoint(method="get")]
@@ -33,4 +37,48 @@ def test_basic():
     http: Client = boot.app.client
     response: TestResponse = http.get("/hello/world")
 
+    assert response.headers["x-test"] == "hello"
+
+
+def test_variable_route():
+    """Should work with variable routes.
+    """
+    class C1(HttpController):
+        ROUTE = "/{id}"
+        ENDPOINTS = [Endpoint(method="get")]
+
+        def get(self, id: str):
+            return {"value": id}
+
+    boot: Boot = Boot(Module(
+        route="/",
+        Controllers=[C1],
+        Middleware=[Mw1]
+    ))
+    http: Client = boot.app.client
+    response: TestResponse = http.get("/e201")
+
+    assert response.json()["value"] == "e201"
+    assert response.headers["x-test"] == "hello"
+
+
+def test_file_path_route():
+    """Should be OK with file paths.
+    """
+    class C1(HttpController):
+        ROUTE = "/{file_path:path}"
+        ENDPOINTS = [Endpoint(method="get")]
+
+        def get(self, file_path: str):
+            return {"value": file_path}
+
+    boot: Boot = Boot(Module(
+        route="/",
+        Controllers=[C1],
+        Middleware=[Mw1]
+    ))
+    http: Client = boot.app.client
+    response: TestResponse = http.get("/doc/pdf/1234.pdf")
+
+    assert response.json()["value"] == "doc/pdf/1234.pdf"
     assert response.headers["x-test"] == "hello"
