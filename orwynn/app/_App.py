46d079efde4179be6311709f4a81519cd70435a6
ryzhovalex <thed4rkof@gmail.com>
2023-05-22 11:52:37 +0300

    feat: App.core_app property

--- orwynn/app/_App.py
+++ orwynn/app/_App.py
@@ -11,36 +11,37 @@ from orwynn.testing import Client, EmbeddedTestClient
 
 class App(FrameworkService):
     def __init__(self, config: AppConfig) -> None:
-        self.__core_app: CoreApp = CoreApp(
+        self._core_app: CoreApp = CoreApp(
             docs_url=config.docs_route,
             redoc_url=config.redoc_route
         )
 
-        self._fw_register_middleware = self.__core_app.add_middleware
+        self._fw_register_middleware = self._core_app.add_middleware
         self._fw_register_exception_handler_fn = \
-            self.__core_app.add_exception_handler
-        self._fw_websocket_handler = self.__core_app.websocket
+            self._core_app.add_exception_handler
+        self._fw_websocket_handler = self._core_app.websocket
 
         self.HTTP_METHODS_TO_REGISTERING_FUNCTIONS: \
             dict[RequestMethod, Callable] = {
-                RequestMethod.GET: self.__core_app.get,
-                RequestMethod.POST: self.__core_app.post,
-                RequestMethod.PUT: self.__core_app.put,
-                RequestMethod.DELETE: self.__core_app.delete,
-                RequestMethod.PATCH: self.__core_app.patch,
-                RequestMethod.OPTIONS: self.__core_app.options
+                RequestMethod.GET: self._core_app.get,
+                RequestMethod.POST: self._core_app.post,
+                RequestMethod.PUT: self._core_app.put,
+                RequestMethod.DELETE: self._core_app.delete,
+                RequestMethod.PATCH: self._core_app.patch,
+                RequestMethod.OPTIONS: self._core_app.options
             }
 
-        self.__client: Client = Client(EmbeddedTestClient(self.__core_app))
-
-        # Remove FastAPI default exception handlers to not cross with ours -
-        # since we write handlers directly via middleware
+        self._client: Client = Client(EmbeddedTestClient(self._core_app))
 
     async def __call__(
         self, scope: Scope, receive: Receive, send: Send
     ) -> None:
-        await self.__core_app(scope, receive, send)
+        await self._core_app(scope, receive, send)
+
+    @property
+    def core_app(self) -> CoreApp:
+        return self._core_app
 
     @property
     def client(self) -> Client:
-        return self.__client
+        return self._client
