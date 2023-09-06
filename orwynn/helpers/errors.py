a4294a64a99e18ae3799ab1edf8ca5d582a49c8d
ryzhovalex <thed4rkof@gmail.com>
2023-09-06 13:29:48 +0300

    chore: antievil

--- orwynn/helpers/errors.py
+++ orwynn/helpers/errors.py
@@ -1,25 +1,5 @@
 from typing import Any
-
-
-class UnsupportedError(Exception):
-    """
-    Some value is not recozniged/supported by the system.
-    """
-    def __init__(
-        self,
-        *,
-        title: str | None = None,
-        value: Any
-    ) -> None:
-        message: str
-
-        pre_title: str = ""
-        if title is not None:
-            pre_title = f"{title}="
-
-        message = f"unsupported value {pre_title}{value}"
-
-        super().__init__(message)
+from antievil import UnsupportedError as UnsupportedError
 
 
 class DeprecatedFeatureError(Exception):
