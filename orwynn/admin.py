9f21ef758e74f3691356d4c0f13909dc7177343c
ryzhovalex <thed4rkof@gmail.com>
2024-02-19 11:14:43 +0300

    add admin codes route

---
+++ orwynn/admin.py
@@ -0,0 +1,10 @@
+from aiohttp import web
+from rxcat import ServerBus
+
+async def handle_get_indexed_codes(request: web.Request) -> web.Response:
+    bus = ServerBus.ie()
+    return web.json_response({
+        "indexedMcodes": bus.IndexedMcodes,
+        "indexedErrcodes": bus.IndexedErrcodes
+    })
+
