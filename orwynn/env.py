4ddbd3ba0159cf68aa22263de607b802fce67916
ryzhovalex <thed4rkof@gmail.com>
2024-07-26 14:19:12 +0300

    update env.py

--- orwynn/env.py
+++ orwynn/env.py
@@ -1,19 +1,17 @@
-from pykit.env import EnvUtils
+from pykit.env import getenv_bool, getenv
 
 
 class OrwynnEnvUtils:
     @classmethod
     def is_debug(cls) -> bool:
-        return EnvUtils.get_bool(key="ORWYNN_DEBUG", default="0")
+        return getenv_bool(key="ORWYNN_DEBUG", default="0").eject()
 
     @classmethod
     def is_clean_allowed(cls) -> bool:
-        return EnvUtils.get_bool(
+        return getenv_bool(
             key="ORWYNN_ALLOW_CLEAN",
-            default="0"
-        )
+            default="0").eject()
 
     @classmethod
     def get_mode(cls) -> str:
-        env = EnvUtils.get(key="ORWYNN_MODE", default="__default__")
-        return env
+        return getenv(key="ORWYNN_MODE", default="__default__").eject()
