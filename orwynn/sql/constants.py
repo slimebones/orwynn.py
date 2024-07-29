987be3f8ca62aa554d11ccd367e1cdfb113e3a8e
ryzhovalex <thed4rkof@gmail.com>
2024-07-29 14:18:47 +0300

    update auth.py, __init__.py, field.py, rbac.py, ...

--- orwynn/sql/constants.py
+++
@@ -1,8 +0,0 @@
-from pykit.cls import Static
-
-
-class PSQLStatementConstants(Static):
-    RecreatePublicSchemaCascade: str = (
-        "DROP SCHEMA public CASCADE;"
-        " CREATE SCHEMA public;"
-    )
