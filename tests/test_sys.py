0f6f1c542fdb13c859422da217fdf323310bd3cd
ryzhovalex <thed4rkof@gmail.com>
2024-08-12 22:43:19 +0300

    update test_sys.py

--- tests/test_sys.py
+++ tests/test_sys.py
@@ -21,23 +21,3 @@ async def test_main(app_cfg: AppCfg):
     r = (await Bus.ie().pubr(
         Mock_1(key="hello"), PubOpts(pubr_timeout=1))).eject()
     assert isinstance(r, ok)
-
-async def test_incorrect_name(app_cfg: AppCfg):
-    async def mock(inp: SysInp[Mock_1, MockCfg]) -> Res[SysInp]:
-        assert inp.msg.key == "hello"
-        return inp.ok()
-
-    plugin = Plugin(
-        name="test", cfgtype=MockCfg, sys=[SysSpec.new(Mock_1, mock)]
-    )
-    app_cfg.plugins.append(plugin)
-    await App().init(app_cfg)
-
-    try:
-        (await Bus.ie().pubr(
-            Mock_1(key="hello"), PubOpts(pubr_timeout=0.2))).eject()
-    except TimeoutError:
-        # no systems were registered => ok
-        pass
-    else:
-        raise AssertionError
