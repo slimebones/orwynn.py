ae3c3e1c341df41ae87fa9a5b034848a007889c1
ryzhovalex <thed4rkof@gmail.com>
2024-08-12 22:57:54 +0300

    update test_opts.py

--- tests/test_opts.py
+++ tests/test_opts.py
@@ -8,10 +8,20 @@ def test_merge():
     async def f3(inp) -> Res: ...
 
     d1 = {
-        "pipeline": AsyncPipeline(f1, f2)
+        "pipeline": AsyncPipeline(f1, f2),
+        "nested": {
+            "pipeline": AsyncPipeline(f3)
+        }
     }
     d2 = {
-        "pipeline": AsyncPipeline(f3)
+        "pipeline": AsyncPipeline(f3),
+        "nested": {
+            "pipeline": AsyncPipeline(f2, f1)
+        }
     }
 
     d3 = _merge(d1, d2).eject()
+    pipeline: AsyncPipeline = d3["pipeline"]
+    assert list(pipeline) == [f1, f2, f3]
+    nested_pipeline: AsyncPipeline = d3["nested"]["pipeline"]
+    assert list(nested_pipeline) == [f3, f2, f1]
